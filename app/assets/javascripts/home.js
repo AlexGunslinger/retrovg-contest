5428eca12f6616b4b884f857f230294658e29654
Alejandro Carmona <acarmona22@gmail.com>
2014-10-13 14:34:08 -0700

    Fix style and game blocks for mobile devices.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,27 @@
 $( document ).ready(function() {
   (function() {
     if( $('.game-list').length ){
+      var winWidth = $(window).width();
+      var conWidth;
+      if(winWidth < 660) {
+        conWidth = 440;
+        col = 2
+      } else if(winWidth < 880) {
+        conWidth = 660;
+        col = 3
+      } else if(winWidth < 1100) {
+        conWidth = 880;
+        col = 4;
+      } else {
+        conWidth = 1100;
+        col = 5;
+      }
+      $('.games-container').width(conWidth);
       $('.games-container').BlocksIt({
-        numOfCol: 4
-      });
+          numOfCol: col,
+          offsetX: 8,
+          offsetY: 8
+        });
     }
   })();
-
 });
