7a150c6556e5fab5001fac612bd74f456886aa4a
Alejandro Carmona <acarmona22@gmail.com>
2014-10-15 10:56:14 -0700

    Now it should work in case of a tie.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   has_many :submissions
-  scope :scoreboard, ->(game_id) { joins(:submissions).select("users.id, users.name, MAX(score) as max_score, MAX(submissions.created_at) as creat_at").where("submissions.game_id = ?", game_id).order("max_score DESC, creat_at DESC").group("users.id, users.name")}
+  scope :scoreboard, ->(game_id) { joins(:submissions).select("users.id, users.name, MAX(score) as max_score, MAX(submissions.created_at) as creat_at").where("submissions.game_id = ?", game_id).order("max_score DESC, creat_at ASC").group("users.id, users.name")}
 end
