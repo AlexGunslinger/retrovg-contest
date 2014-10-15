6f0ef93135cdb5ee539b76cf5df8978996ee2f39
Alejandro Carmona <acarmona22@gmail.com>
2014-10-14 20:14:53 -0700

    Submission form now remember the last user selected.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -3,13 +3,14 @@ class SubmissionsController < ApplicationController
   before_filter :fetch_players_and_games, only: [:new, :create]
 
   def new
-    @submission = Submission.new
+    @submission = Submission.new(user_id: cookies[:player_id])
   end
 
   def create
     @submission = Submission.new(submission_params)
     @submission.ip_address = request.remote_ip
     if @submission.save
+      set_player_cookie
       redirect_to scoreboard_game_path(@submission.game), notice: "Good Luck!"
     else
       render "new"
@@ -23,8 +24,14 @@ class SubmissionsController < ApplicationController
   end
   
   def fetch_players_and_games
-    @players    = User.order('name ASC')
-    @games      = Game.order('name ASC')
+    @players = User.order('name ASC')
+    @games   = Game.order('name ASC')
   end
 
+  def set_player_cookie
+    if cookies[:player_id] != @submission.user_id
+      cookies[:player_id] = @submission.user_id
+    end
+  end
+  
 end
