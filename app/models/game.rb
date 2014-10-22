b32cf5112032036d7f2263ba3a6208ec9ce5581f
Alejandro Carmona <acarmona22@gmail.com>
2014-10-22 01:33:05 -0700

    Order the games by name in player best scores.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ActiveRecord::Base
   has_many :submissions
-  scope :scoreboard, -> (user_id) { joins(:submissions).select("games.id, games.name, MAX(score) as max_score, MAX(submissions.created_at) as creat_at").where("submissions.user_id = ?", user_id).order("max_score DESC, creat_at ASC").group("games.id, games.name")}
+  scope :scoreboard, -> (user_id) { joins(:submissions).select("games.id, games.name, MAX(score) as max_score, MAX(submissions.created_at) as creat_at").where("submissions.user_id = ?", user_id).order("games.name ASC").group("games.id, games.name")}
 end
