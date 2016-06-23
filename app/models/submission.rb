a670c01ea3dd864ed985276ef33d1f769100ac0a
Alejandro Carmona <acarmona22@gmail.com>
2016-06-22 20:39:16 -0500

    Where is my head? Scope in model

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,7 +2,9 @@ class Submission < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
   validates_presence_of :user_id, :game_id
-  validates :score, numericality: { only_integer: true, message: "Should be a number!" } 
+  validates :score, numericality: { only_integer: true, message: "Should be a number!" }
+
+  scope :tournament_one, -> { where("user_id < 74") }
 
   LATEST = 7
 end
