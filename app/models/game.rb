e5b70424be8f782b4d9b8e5dbbda0bf260b982b1
Alejandro Carmona <acarmona22@gmail.com>
2016-06-22 20:33:32 -0500

    Small mistake, is mysql not ruby

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
   has_many :submissions
   scope :tournament_one, -> { where(tournament_id: nil) }
-  scope :scoreboard, -> (user_id) { joins(:submissions).select("games.id, games.name, MAX(score) as max_score, MAX(submissions.created_at) as creat_at").where("submissions.user_id = ? && games.tournament_id IS NULL", user_id).order("games.name ASC").group("games.id, games.name")}
+  scope :scoreboard, -> (user_id) { joins(:submissions).select("games.id, games.name, MAX(score) as max_score, MAX(submissions.created_at) as creat_at").where("submissions.user_id = ? AND games.tournament_id IS NULL", user_id).order("games.name ASC").group("games.id, games.name")}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
 
   scope :tournament_one, -> { where(tournament_id: nil) }
 
-  scope :scoreboard, -> (game_id) { joins(:submissions).select("users.id, users.name, MAX(score) as max_score, MAX(submissions.created_at) as creat_at").where("submissions.game_id = ? && users.tournament_id IS NULL", game_id).order("max_score DESC, creat_at ASC").group("users.id, users.name")}
+  scope :scoreboard, -> (game_id) { joins(:submissions).select("users.id, users.name, MAX(score) as max_score, MAX(submissions.created_at) as creat_at").where("submissions.game_id = ? AND users.tournament_id IS NULL", game_id).order("max_score DESC, creat_at ASC").group("users.id, users.name")}
 end
